af0f7407e97c449852106ec2443178e39d0754b5
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-07-13 00:11:45 -0300

    adicionado a pasta v1 na pasta controllers do projeto onthefly.airportservice

--- OnTheFly.AirportService/Controllers/v1/AirportController.cs
+++ OnTheFly.AirportService/Controllers/v1/AirportController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using OnTheFly.Connections;
 using OnTheFly.Models;
 
-namespace OnTheFly.AirportService.Controllers
+namespace OnTheFly.AirportService.Controllers.v1
 {
     [Route("[controller]")]
     [ApiController]
@@ -22,17 +22,17 @@ namespace OnTheFly.AirportService.Controllers
         [HttpGet("/ByIATA/{iata}", Name = "GetAirportIata")]
         public ActionResult<Airport> Get(string iata)
         {
-            Airport? airport = _airport.Get(iata);
+            Airport airport = _airport.Get(iata);
             if (airport == null)
                 return NotFound();
 
-            State? state = _state.GetUF(airport.State);
+            State state = _state.GetUF(airport.State);
             if (state == null) airport.State = "null";
             else airport.State = state.UF;
 
             return airport;
         }
-        
+
         [HttpGet("/ByState/{state}", Name = "GetAirportState")]
         public ActionResult<List<Airport>> GetByState(string state)
         {
