5f7c95ec390c9850864aa0bf8c4a73d1432ee213
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-07-14 01:10:49 -0300

    corrigido o metodo de insert da classe aircraftservice para insertaircraftasync

--- OnTheFly.CompanyService/Services/v1/AirCraftService.cs
+++ OnTheFly.CompanyService/Services/v1/AirCraftService.cs
@@ -2,12 +2,13 @@
 using OnTheFly.Models;
 using OnTheFly.Models.DTO;
 using System.Text;
+
 namespace OnTheFly.CompanyService.Services.v1
 {
     public class AircraftService
     {
         private HttpClient _httpClient = new HttpClient();
-        public async Task<AirCraft> InsertAircraft(AirCraftDTO airCraftDTO)
+        public async Task<AirCraft> InsertAircraftAsync(AirCraftDTO airCraftDTO)
         {
             HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(airCraftDTO), Encoding.UTF8, "application/json");
             HttpResponseMessage res = await _httpClient.PostAsync("https://localhost:5000/api/AirCraft/", httpContent);
