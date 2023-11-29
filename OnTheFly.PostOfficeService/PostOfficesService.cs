23ebcb23c45ebed0344d901f2d34b974aeac4445
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-11-29 17:17:50 -0300

    Padronizacao dos nomes dos metodos da classe PostOfficesService

--- OnTheFly.PostOfficeService/PostOfficesService.cs
+++ OnTheFly.PostOfficeService/PostOfficesService.cs
@@ -1,27 +1,25 @@
 ﻿using Newtonsoft.Json;
 using OnTheFly.Models;
-using OnTheFly.Models.DTO;
 
-namespace OnTheFly.PostOfficeService
+namespace OnTheFly.PostOfficeService;
+
+public class PostOfficesService
 {
-    public class PostOfficesService
-    {
-        static readonly HttpClient address = new HttpClient();
+    static readonly HttpClient address = new HttpClient();
 
-        public async Task<Address> GetAddress(string zipCode)
+    public async Task<Address> GetAddressAsync(string zipCode)
+    {
+        try
         {
-            try
-            {
-                HttpResponseMessage response = await address.GetAsync("https://viacep.com.br/ws/" + zipCode + "/json/");
-                response.EnsureSuccessStatusCode();
-                string addressResponse = await response.Content.ReadAsStringAsync();
-                var ad = JsonConvert.DeserializeObject<Address>(addressResponse);
-                return ad;
-            }
-            catch (HttpRequestException e)
-            {
-                return null;
-            }
+            HttpResponseMessage response = await address.GetAsync("https://viacep.com.br/ws/" + zipCode + "/json/");
+            response.EnsureSuccessStatusCode();
+            string addressResponse = await response.Content.ReadAsStringAsync();
+            var ad = JsonConvert.DeserializeObject<Address>(addressResponse);
+            return ad;
+        }
+        catch (HttpRequestException e)
+        {
+            return null;
         }
     }
 }
